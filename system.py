e881580a492d9f825815509571303c95592f9ed6
Ricetia <babapass742@gmail.com>
2021-03-15 19:31:00 +0800

    整理路徑

---
+++ system.py
@@ -0,0 +1,10 @@
+from flask import Flask #載入Flask
+app = Flask(__name__) #建立Application物件
+
+#建立網站首頁的回應方式
+@app.route("/")
+def index(): #用來回應網站首頁連線的方式
+    return "Hello Flask" #回傳網站首頁的內容
+
+#啟動網站伺服器
+app.run()
