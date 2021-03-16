2c83622f7219852535fe47d641d4e28f3c401ec0
Ricetia <babapass742@gmail.com>
2021-03-16 17:34:58 +0800

    初步測試完成

--- system.py
+++ system.py
@@ -1,10 +1,50 @@
-from flask import Flask #載入Flask
-app = Flask(__name__) #建立Application物件
+from flask import Flask 
+from flask import request
+from flask import render_template
+from flask import session
 
-#建立網站首頁的回應方式
+app = Flask(__name__,static_folder="static",static_url_path="/")
+app.config['SECRET_KEY'] = 'ricetia' 
+
 @app.route("/")
-def index(): #用來回應網站首頁連線的方式
-    return "Hello Flask" #回傳網站首頁的內容
+def index(): 
+    if 'IDnumber' in session:
+        return render_template("member.html")
+    return render_template("system.html")
+
+@app.route("/member")
+def member():
+    if 'IDnumber' not in session:
+        return render_template("system.html")
+    return render_template("member.html")
+
+@app.route("/error")
+def error():
+    return render_template("error.html")
+
+@app.route("/signin", methods=["POST"])
+def signin ():
+    IDnumber = request.form["IDnumber"]
+    password = request.form["password"]
+    whitelist = {"test":"test"}
+    for key,value in whitelist.items():
+        if IDnumber == key :
+            if password == value :
+                session["IDnumber"] = IDnumber
+                session["password"] = password
+                return render_template("member.html")
+            else :
+                return render_template("error.html")
+        else :
+            return render_template("error.html")
+
+@app.route("/signout",methods=["POST"])
+def signout ():
+    del session["IDnumber"]
+    del session["password"]
+    return render_template("system.html")
+
+
 
 #啟動網站伺服器，可透過port參數指定埠號
 app.run(port=3000)
