f7fe4eb1af47a850874c36f312baff51f2b9ff70
Ricetia <babapass742@gmail.com>
2021-04-05 16:09:39 +0800

    整理變數，改網頁架構

--- system.py
+++ system.py
@@ -5,28 +5,39 @@ from flask import session
 import mysql.connector
 import pymysql.cursors
 import pymysql
-# from django.db import connection
 
 app = Flask(__name__,static_folder="static",static_url_path="/")
 app.config['SECRET_KEY'] = 'ricetia' 
 
+#首頁變數(註冊區)
+# registered_name = request.form["registered_name"]
+# registered_username = request.form["registered_username"]
+# registered_password = request.form["registered_password"]
+#首頁變數(登錄區)
+# username = request.form["username"]
+# password = request.form["password"]
+#會員頁變數(查詢會員姓名)
+# check_name = request.form["check_name"]
+#會員頁變數(更新我的姓名)
+# updata_name = request.form["updata_name"]
+
 @app.route("/")
 def index(): 
-    if 'username' in session:
-        return render_template("member.html")
-    return render_template("system.html")
+    if 'name' in session:
+        return render_template("member.html",data=session["name"])
+    else :
+        return render_template("system.html")
 
 @app.route("/member")
 def member():
-    name = session["name"]
     if 'username' not in session:
         return render_template("system.html")
-    return render_template("member.html",data=name)
+    else :
+        return render_template("member.html",data=session["name"])
 
-@app.route("/error",methods=['GET'])
+@app.route("/error")
 def error():
-    message = session["error"]
-    return render_template("error.html",data=message)
+    return render_template("error.html")
 
 @app.route("/signin", methods=["POST"])
 def signin ():
@@ -40,19 +51,17 @@ def signin ():
         db='website',
         )
     with signup.cursor() as cursor:
-        mysqlsave = "SELECT `password`,`name` FROM `user` WHERE `username`=%s"
-        cursor.execute(mysqlsave,username)
+        mysqlact = "SELECT `password`,`name` FROM `user` WHERE `username`=%s"
+        cursor.execute(mysqlact,username)
         result = cursor.fetchall()
     signup.close()
 
     if len(result) > 0 :
-        test_password = result[0][0]
-        name = result[0][1]
-        if test_password == password :
-            session["name"] = name
+        if result[0][0] == password :
+            session["name"] = result[0][1]
             session["username"] = username
             session["password"] = password
-            return render_template("member.html",data=name)
+            return render_template("member.html",data=session["name"])
         else :
             error = "帳號或密碼輸入錯誤"
             session["error"] = error
@@ -72,6 +81,10 @@ def signout ():
 
 @app.route("/signup", methods=["POST"])
 def signup ():
+    registered_name = request.form["registered_name"]
+    registered_username = request.form["registered_username"]
+    registered_password = request.form["registered_password"]
+
     signup = pymysql.connect(
         host='localhost',
         user='root',
@@ -79,33 +92,21 @@ def signup ():
         db='website',
         cursorclass=pymysql.cursors.DictCursor #以字典方式儲存
         )
-
-    name = request.form["username"]
-    username = request.form["registered_username"]
-    password = request.form["registered_password"]
-
     with signup.cursor() as cursor:
         mysqldata = "SELECT `username` FROM `user` WHERE `username`=%s"
-        cursor.execute(mysqldata,username)
-        whitelist = cursor.fetchall()
-    signup.close()
-
-    if len(whitelist) < 1 :
-        signup = pymysql.connect(
-        host='localhost',
-        user='root',
-        password='ricetia',
-        db='website',
-        )
+        cursor.execute(mysqldata,registered_username)
+        result = cursor.fetchall()
+    
+    if len(result) < 1 :
         with signup.cursor() as cursor:
-            mysqlsave = "INSERT INTO user (name,username,password) VALUES (%s,%s,%s)"
-            cursor.execute(mysqlsave,(name,username,password))
-            signup.commit()
-        signup.close()
+            mysqlact = "INSERT INTO user (name,username,password) VALUES (%s,%s,%s)"
+            cursor.execute(mysqlact,(registered_name,registered_username,registered_password))
+            signup.commit() #資料上傳
         return render_template("system.html")
     else :
         error = "帳號已經被註冊過"
         return render_template("error.html",data=error)
+    
+    signup.close() #把資料庫關起來
 
-  
 app.run(port=3000)
